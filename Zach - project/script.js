b43c0ca70da49d4d46d959ec24a1fce146b3de4c
Trinh Nguyen <blossom90vt@hotmail.com>
2020-03-24 17:09:25 -0500

    added directions

--- Zach - project/script.js
+++ Zach - project/script.js
@@ -22,3 +22,21 @@ $(".btn").on("click",function(event){
           })
   }
 })
+
+
+//Country array
+//button click function {
+  //preventDefault to avoid submitting form;
+  //get the value of new country in searchform box, and assign value to a variable
+  //push the new country variable to country array
+  //for loop to loop through country array
+    // assign array[i] to a new variable,
+    //assign the URL link to a countryURL variable, and add the array[i] variable into to URL link
+// $.ajax({
+//               url: countryURL,
+//               method: "GET"
+//           }).then(function (response) {
+//             //Display country into on page
+//           }
+// }
+
