ad985e7f5b27f4dc7274fa83b2f9f84ec399316f
Trinh Nguyen <blossom90vt@hotmail.com>
2020-03-27 13:36:44 -0500

    added more notes

--- Tobi - this/script.js
+++ Tobi - this/script.js
@@ -1,3 +1,4 @@
+//Dynamically create a button for each search
 $(".btn").on("click", function () {
   var btn = $("<button>")
   var inputVal = $(".inputVal").val().trim();
@@ -8,14 +9,14 @@ $(".btn").on("click", function () {
   $(".container").append(btn)
 })
 
-//CORRECT WAY
+//CORRECT WAY to get value of a specific button when it's clicked
 $(document).on("click","button", function(){
   var btnVal = $(this).attr("value");
   console.log(btnVal);
 })
 
-//WRONG WAY
-$("button").on("click", function(){
+//WRONG WAY because not, targeting the correct button
+$(document).on("click","button", function(){
   var btnVal = $(".btnClass").attr("value")
   console.log(btnVal)
 })
